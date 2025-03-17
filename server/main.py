dc8d661cbfa2e3988f596de2abac657e0f282d71
HalkatHaathi <roybarnamoy13@gmail.com>
2025-03-18 01:25:20 +0530

    added redis

--- server/main.py
+++ server/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from routes import router as main_router
+import redis.asyncio as redis
+import os
 
 app = FastAPI()
+
+# Redis Connection
+REDIS_URL = "redis://localhost:6379"
+redis_client = redis.from_url(REDIS_URL, decode_responses=True)
 
 # Include all routes
 app.include_router(main_router)
