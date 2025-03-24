8359310895c5596f6564c84be44ff7a988d2e90d
HalkatHaathi <roybarnamoy13@gmail.com>
2025-03-25 01:52:48 +0530

    small changes for deployment

--- client/components/section-card.tsx
+++ client/components/section-card.tsx
@@ -1,5 +1,4 @@
-import {TimerResetIcon, TrendingUp } from "lucide-react"
-import { Badge } from "@/components/ui/badge"
+import { TrendingUp } from "lucide-react"
 import {
   Card,
   CardDescription,
